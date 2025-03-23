df8d8b5a2dd9953a8a0be63fca78b5ec8f5a570a
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-03-23 07:49:22 -0300

    feat(seeder): add appointment factory, update comments and record count

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,45 +6,41 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
 use App\Models\Barber;
+use App\Models\Appointment;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        /**
-         * Truncate tables.
-         */
+        // Disable foreign key checks.
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        // Truncate tables.       
         DB::table('users')->truncate();
         DB::table('barbers')->truncate();
+        DB::table('appointments')->truncate();
+        // Enable foreign key checks.
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
-        /**
-         * Create a specific user with admin role.
-         */
+        // Create a specific user with admin role.
         User::factory()->create([
             'name' => 'Test Admin',
             'email' => 'federicogildemuro@gmail.com',
             'role' => 'admin',
         ]);
 
-        /**
-         * Create a specific user with user role.
-         */
+        // Create a specific user with user role.
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'fgildemuro@hotmail.com',
         ]);
 
-        /**
-         * Create 10 random users with user role.
-         */
-        User::factory(10)->create();
+        // Create 50 random users.
+        User::factory(50)->create();
 
-        /**
-         * Create 10 random barbers.
-         */
-        Barber::factory(10)->create();
+        // Create 2 random barbers.
+        Barber::factory(2)->create();
+
+        // Create 100 random appointments.
+        Appointment::factory(100)->create();
     }
 }
