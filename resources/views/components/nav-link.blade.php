0811962eb4838d679fa3a560d1084ac35d74c092
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-04-09 09:24:02 -0300

    style(nav-link): update active state styling

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,7 +3,7 @@
 @php
     $classes =
         $active ?? false
-            ? 'inline-flex items-center font-bold scale-110 hover:text-yellow transition duration-150 ease-in-out'
+            ? 'inline-flex items-center font-bold scale-110 text-yellow'
             : 'inline-flex items-center hover:text-yellow transition duration-150 ease-in-out';
 @endphp
 
