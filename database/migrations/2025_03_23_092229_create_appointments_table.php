8add7b6844646b4e7c8adb1343fbc9d5ca264367
Federico Gil de Muro <federicogildemuro@gmail.com>
2025-04-11 15:29:39 -0300

    refactor(appointments): change barber_id fk to restrict on delete

--- database/migrations/2025_03_23_092229_create_appointments_table.php
+++ database/migrations/2025_03_23_092229_create_appointments_table.php
@@ -1,29 +1,23 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('appointments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('barber_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->nullable()->default(null)->constrained()->onDelete('set null');
+            $table->foreignId('barber_id')->constrained();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
             $table->date('date');
             $table->time('time');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('appointments');
